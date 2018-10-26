3a2d080af7979e6444da3c23a9c2017673a84a0d
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-10-25 20:17:36 -0700

    Initial Commit

---
+++ ikid/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ikid
+//
+//  Created by Nattanon Bunyatipanon on 10/25/18.
+//  Copyright © 2018 Nattanon Bunyatipanon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ikid/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ikid
+//
+//  Created by Nattanon Bunyatipanon on 10/25/18.
+//  Copyright © 2018 Nattanon Bunyatipanon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
